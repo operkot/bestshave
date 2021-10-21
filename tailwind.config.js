a2c5f4e5f76d772f99cb3ff9ccc1b52e8b6a680b
Aleksandr Spitsin <aleks.spitsin@gmail.com>
2021-10-21 23:48:36 +0300

    Dashboard pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   purge: {
     enabled: true,
     content: ['src/**/*.html', 'src/**/*.pug', 'src/**/*.js'],
-    safelist: ['mb-6', 'mb-8', 'pt-6', 'pb-20', 'text-center'],
+    safelist: [
+      'mb-6',
+      'mb-8',
+      'pt-6',
+      'pb-20',
+      'px-8',
+      'text-center',
+      'text-sm',
+      'md:w-auto',
+    ],
   },
   theme: {
     screens: {
